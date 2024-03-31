82f971986f1a8fc7109c2801a9c6ab0bf64f66a3
JG Gouveia <joao.gabriel.gouveia2@gmail.com>
2024-03-31 14:56:52 -0300

    feat: add executor Input definition

---
+++ pkg/executor/definition.go
@@ -0,0 +1,40 @@
+package executor
+
+import (
+	"time"
+)
+
+type Input struct {
+	Values                 []string
+	TempDir                string
+	WorkDir                string
+	Main                   string
+	Cmd                    []string
+	OnStartCallback        func(c OnStartCallbackData)
+	OnEachCompleteCallback func(c OnEachCompleteCallbackData)
+	OnFinishCallback       func(c OnFinishCallbackData)
+}
+
+type OnStartCallbackData struct {
+	Id        string
+	StartTime time.Time
+	Main      string
+	DirSize   int64
+}
+
+type OnEachCompleteCallbackData struct {
+	Id         string
+	Ref        string
+	StartTime  time.Time
+	FinishTime time.Time
+	TotalTime  int64
+	Output     string
+	StatusCode int8
+	Error      bool
+}
+
+type OnFinishCallbackData struct {
+	Id         string
+	FinishTime time.Time
+	TotalTime  int64
+}
