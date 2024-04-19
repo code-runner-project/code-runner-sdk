de6f60085d2c4e8b78d6b8510e4e13dce1e96ad6
JG Gouveia <joao.gabriel.gouveia2@gmail.com>
2024-04-19 20:36:46 -0300

    refactor!: remove orchestrator and use only context

--- pkg/context/definition.go
+++ pkg/context/definition.go
@@ -1,7 +1,10 @@
 package context
 
 import (
-	"github.com/code-runner-project/code-runner-sdk/pkg/orchestra"
+	"github.com/code-runner-project/code-runner-sdk/pkg/executor"
+	"github.com/code-runner-project/code-runner-sdk/pkg/receiver"
+	"github.com/code-runner-project/code-runner-sdk/pkg/runner"
+	"github.com/code-runner-project/code-runner-sdk/pkg/transmitter"
 )
 
 type Message struct {
@@ -9,13 +12,41 @@ type Message struct {
 }
 
 type Context struct {
-	orchestrator *orchestra.Orchestrator
+	Message      Message
+	Runners      map[string]runner.Definition
+	Executors    map[string]executor.Definition
+	Receivers    map[string]receiver.Definition
+	Transmitters map[string]transmitter.Definition
+}
+
+func (o *Context) RegisterRunner(name string, runner runner.Definition) {
+	o.Runners[name] = runner
+}
+
+func (o *Context) RegisterExecutor(name string, executor executor.Definition) {
+	o.Executors[name] = executor
+}
+
+func (o *Context) RegisterReceiver(name string, receiver receiver.Definition) {
+	o.Receivers[name] = receiver
+}
+
+func (o *Context) RegisterTransmitter(name string, transmitter transmitter.Definition) {
+	o.Transmitters[name] = transmitter
 }
 
 func (c *Context) Proceed(message Message) {
-	c.orchestrator.Execute(message)
+	c.Message = message
 }
 
-func NewContext(orchestrator *orchestra.Orchestrator) Context {
-	return Context{orchestrator: orchestrator}
+func NewContext() *Context {
+	return &Context{
+		Message: Message{
+			FlowDefinition: "",
+		},
+		Runners:      make(map[string]runner.Definition),
+		Executors:    make(map[string]executor.Definition),
+		Receivers:    make(map[string]receiver.Definition),
+		Transmitters: make(map[string]transmitter.Definition),
+	}
 }
